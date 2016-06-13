3948874fa0268898101f58a5cef7a9b55d9461c6
Eric Yu <eric.yutao@gmail.com>
2016-06-13 16:50:17 +0800

    add signal file

---
+++ signal/signal.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		panic(fmt.Sprintf("error opening file: %v", err))
+
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println("This is a test log entry")
+
+	log.Println("Catch kill signal")
+	fmt.Println("Wait a kill signal")
+
+	interrupt := make(chan os.Signal, 1)
+	//signal.Notify(interrupt, os.Interrupt)
+	//signal.Notify(interrupt, syscall.SIGTERM)
+	signal.Notify(interrupt, syscall.SIGHUP)
+
+	select {
+	case <-interrupt:
+		log.Println("get kill signal:", time.Now())
+		time.Sleep(time.Second * 10)
+		log.Println("stop time:", time.Now())
+	}
+}

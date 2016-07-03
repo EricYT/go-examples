1183cda9e7c74c3d9afaa044735cc280461ee292
eric yu <eric.yutao@gmail.com>
2016-07-03 12:03:12 +0800

    moidfy sub package use main function flags

--- flags/bar/bar.go
+++ flags/bar/bar.go
@@ -1,10 +1,27 @@
 package bar
 
-var bars string
+import (
+	"flag"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var (
+	bar *string = flag.String("bar", "default bar", "sub package flag init")
+)
 
 func int() {
+	go Display()
 }
 
 func Display() string {
-	return bars
+	for !flag.Parsed() {
+		// Defer execution of this goroutine
+		runtime.Gosched()
+
+		time.Sleep(time.Second * 1)
+	}
+	fmt.Println("bar package bar:", *bar)
+	return *bar
 }
--- flags/flag.go
+++ flags/flag.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
 
 	"github.com/EricYT/go-examples/flags/bar"
 )
@@ -15,16 +14,9 @@ func init() {
 }
 
 func main() {
-	// Define Flags
-	maxp := flag.Int("max", 6, "the max value")
-
-	var strPtr *string
-	strPtr = flag.String("str", "default", "the string")
 	// Parse
 	flag.Parse()
 	// Generate a random value between 0 and max
-	fmt.Println(rand.Intn(*maxp))
-	fmt.Println(*strPtr)
-	fmt.Println(foo)
+	fmt.Println("main function:", foo)
 	fmt.Println("bar is ", bar.Display())
 }

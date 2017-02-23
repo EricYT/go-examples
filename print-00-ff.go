e592dce8fa37fc7f34ee887ece5c2c39ace991b3
Eric Yu <eric.yutao@gmail.com>
2017-02-23 09:47:53 +0800

    add print 00-ff %02x format

--- print-00-ff.go
+++ print-00-ff.go
@@ -9,6 +9,8 @@ func main() {
 	var value uint8
 	var vs string
 	for index := 0; index <= max; index++ {
+		// its better to use
+		// fmt.Printf("index %d 0x: %02x\n", index, index)
 		value = uint8(index)
 		vs = fmt.Sprintf("%x%x", value>>4&0xf, value&0xf)
 		fmt.Printf("index %d 0x: %s\n", value, vs)

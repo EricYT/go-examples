67098b6dc75bda06e68777e21850eee980351301
Eric Yu <eric.yutao@gmail.com>
2017-07-10 17:41:56 +0800

    add hex encoding codes

---
+++ encoding/hex.go
@@ -0,0 +1,61 @@
+package encoding
+
+import (
+	"errors"
+	"fmt"
+)
+
+// hex bytes
+var hextable []byte = []byte{
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	'a', 'b', 'c', 'd', 'e', 'f',
+}
+
+// Encode implements hexadecimal encoding.
+func Encode(dst, src []byte) int {
+	for i, v := range src {
+		dst[i*2] = hextable[v>>4]
+		dst[i*2+1] = hextable[v&0x0F]
+	}
+	return len(src) * 2
+}
+
+func EncodeToString(src []byte) string {
+	dst := make([]byte, len(src)*2)
+	Encode(dst, src)
+	return string(dst)
+}
+
+var ErrWrongLen error = errors.New("decode source bytes length is wrong")
+
+func Decode(dst, src []byte) (int, error) {
+	if len(src)%2 == 1 {
+		return 0, ErrWrongLen
+	}
+
+	for i := 0; i < len(src)/2; i++ {
+		a, ok := fromHexChar(src[i*2])
+		if !ok {
+			return 0, fmt.Errorf("encoding/hex: invalid byte(%#U)", src[i*2])
+		}
+		b, ok := fromHexChar(src[i*2+1])
+		if !ok {
+			return 0, fmt.Errorf("encoding/hex: invalid byte(%#U)", src[i*2+1])
+		}
+		dst[i] = (a << 4) | b
+	}
+
+	return len(src) / 2, nil
+}
+
+func fromHexChar(c byte) (byte, bool) {
+	switch {
+	case c >= '0' && c <= '9':
+		return c - '0', true
+	case c >= 'a' && c <= 'f':
+		return c - 'a' + 10, true
+	case c >= 'A' && c <= 'F':
+		return c - 'A' + 10, true
+	}
+	return 0, false
+}
---
+++ encoding/hex_test.go
@@ -0,0 +1,81 @@
+package encoding
+
+import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"log"
+	"testing"
+)
+
+var src []byte
+var dstStr string
+
+func init() {
+	tmp := md5.Sum([]byte("hello, hex encoding"))
+	src = tmp[:]
+	dstStr = easyWayEncodeHex(src)
+
+	log.Printf("src(%v) dst string(%s)\n", src, dstStr)
+}
+
+func easyWayEncodeHex(src []byte) string {
+	var dst string
+	for _, v := range src {
+		dst += fmt.Sprintf("%02x", v)
+	}
+	return dst
+}
+
+func TestEncode(t *testing.T) {
+	dst := make([]byte, len(src)*2)
+	count := Encode(dst, src)
+	if count != len(src)*2 {
+		t.Fatalf("Encode hex src(%v) to dst(%v) count not equal", src, dst, count)
+	}
+	if string(dst) != dstStr {
+		t.Fatalf("Encode hex src(%v) to hex dst(%s) wrong with (%s)", src, string(dst), dstStr)
+	}
+}
+
+func TestEncodeToString(t *testing.T) {
+	dst := EncodeToString(src)
+	if dst != dstStr {
+		t.Fatalf("Encode to string src(%v) dst string(%s) not same as (%s)", src, dst, dstStr)
+	}
+}
+
+func TestDecode(t *testing.T) {
+	hex := make([]byte, len(src)*2)
+	Encode(hex, src)
+
+	dst := make([]byte, len(hex)/2)
+	count, err := Decode(dst, hex)
+	if err != nil {
+		t.Fatalf("Decode src(%s) to original bytes(%v) error: %s", string(hex), src, err)
+	}
+	if count != len(hex)/2 {
+		t.Fatalf("Decode src(%s) decode to wrong length: %d", string(hex), count)
+	}
+	if bytes.Compare(dst, src) != 0 {
+		t.Fatalf("Decode src(%s) got wrong result(%v) right(%v)", string(hex), dst, src)
+	}
+}
+
+func TestDecodeWrongSrcLen(t *testing.T) {
+	src := []byte{89, 34, 23, 1, 0}
+	dst := make([]byte, len(src)*2)
+	_, err := Decode(dst, src)
+	if err == nil {
+		t.Fatalf("Decode src(%v) is wrong hex format with length", src)
+	}
+}
+
+func TestDecodeWrongSrc(t *testing.T) {
+	src := []byte{'i', 12, 34, 23, 1, 0}
+	dst := make([]byte, len(src)*2)
+	_, err := Decode(dst, src)
+	if err == nil {
+		t.Fatalf("Decode src(%v) is wrong hex format", src)
+	}
+}
